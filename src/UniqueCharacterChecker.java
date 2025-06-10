df9b182c74f10213c03e3b6de245f4886b754dc2
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-06-10 13:01:40 -0700

    Unique Character

--- src/UniqueCharacterChecker.java
+++ src/UniqueCharacterChecker.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /**
  * The UniqueCharacterChecker class provides a method to check if all characters
  * in a given word are unique.
@@ -12,9 +14,22 @@ public class UniqueCharacterChecker {
      */
     public static boolean hasUniqueCharacters(String word) {
         // TODO: implement this!
+        // Create a HashSet to store characters we've seen
+        HashSet<Character> uniqueChar = new HashSet<>();
+        // Iterate through each character in the word
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            // Check if the character is already in the set
+            if (uniqueChar.contains(currentChar)) {
+                return false; // Duplicate found
+            }
+            // Add the character to the set
+            uniqueChar.add(currentChar);
+        }
         // Requirement: This must run in O(n) time, where n is the number of characters in the word
         // Hint: Stuck? Consider looking up "charAt" and seeing how it can help you
-        return false;
+
+        return true; // All characters are unique
     }
 
     public static void main(String[] args) {
@@ -25,5 +40,7 @@ public class UniqueCharacterChecker {
         System.out.println(word1 + " has unique characters: " + hasUniqueCharacters(word1));
         System.out.println(word2 + " has unique characters: " + hasUniqueCharacters(word2));
         System.out.println(word3 + " has unique characters: " + hasUniqueCharacters(word3));
+
+        
     }
 }
